0f99be979d0047ac91e5824d1adf003bc3f94c9d
ameeryousufalibhatti <yousufbhatti2002@gmail.com>
2024-08-31 17:06:22 +0500

    Vite Config Updated!

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    watch: {
+      usePolling: true,      // Enable polling to reduce the number of file watchers
+      interval: 1000,        // Adjust the polling interval if needed
+      ignored: ['**/node_modules/**'], // Exclude node_modules from being watched
+    },
+  },
 })
